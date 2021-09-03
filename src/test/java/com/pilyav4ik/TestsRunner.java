cdacfa50624c66560ca1f5f59a7c3bbad31e0f73
Igor Piliavskyi <ipiliavskyi@gmail.com>
2021-09-03 19:27:57 +0200

    #6 TC-2 Rerun failed tests

--- src/test/java/com/pilyav4ik/TestsRunner.java
+++ src/test/java/com/pilyav4ik/TestsRunner.java
@@ -2,13 +2,14 @@ package com.pilyav4ik;
 
 import com.pilyav4ik.driver.BaseUtil;
 import com.pilyav4ik.pages.BasePage;
+import com.pilyav4ik.utils.RetryAnalyzer;
 import org.testng.annotations.Test;
 
 
 
 public class TestsRunner extends BaseUtil {
 
-    @Test
+    @Test(retryAnalyzer = RetryAnalyzer.class)
     public void AcceptCookies(){
         BasePage basePage = new BasePage(driver);
         basePage.clickToAcceptCookies();
